229686659a7f6bfd08debaf55e2710b3217c0db7
PARIA AGHARABI <pariaagharabi@gmail.com>
2024-04-03 14:26:17 -0700

    first commit

---
+++ pages/components/OperationButton.jsx
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./CalcApp"
+
+export default function OperationButton({ dispatch, operation }) {
+    return (
+        <button
+            onClick={() =>
+                dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+            }
+        >
+            {operation}
+        </button>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalcApp from "./components/CalcApp"
+
+export default function Home() {
+
+  return (
+    <div className="calc-container">
+      <CalcApp />
+
+    </div>
+  );
+}
